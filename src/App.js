baa26cc48481513d873bac97a6cb445f78521d13
German Nunez <german90nunez@gmail.com>
2020-05-30 16:31:43 -0700

    react router intalled and routes working

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Home from './components/Home';
 import About from './components/About';
 import Cart from './components/Cart';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <Fragment>
-      <Navbar />
-      <Home />
-      <About />
-      <Cart />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/cart" component={Cart} />
+        </Switch>
+      </Router>
     </Fragment>
   );
 }
